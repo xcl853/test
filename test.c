2c32250259b33318b1249e061b88973b4a7e50c3
xcl_linux <chunlin_xcl@163.com>
2020-09-14 15:05:08 +0800

    test01

---
+++ test.c
@@ -0,0 +1,13 @@
+/************************else*************************************************
+> File Name: test.c
+> Author: 夏椿林
+> Mail: chunlin_xia@163.com 
+> Created Time: 2020年09月14日 星期一 15时03分46秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main(int argc, const char *argv[])
+{
+	printf("hello world!");
+	return 0;
+}
