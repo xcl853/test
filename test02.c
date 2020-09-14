c5bbec8fce9508027f64f71455b204bcab1c49bc
xcl_linux <chunlin_xcl@163.com>
2020-09-14 15:17:14 +0800

    test_branch

---
+++ test02.c
@@ -0,0 +1,14 @@
+/************************else*************************************************
+> File Name: test02.c
+> Author: 夏椿林
+> Mail: chunlin_xia@163.com 
+> Created Time: 2020年09月14日 星期一 15时15分51秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("hello github\n");
+	return 0;
+}
